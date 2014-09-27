476b1df81783d26f37bc0198f00ca580f0c8b81c
Ivan Gromakovsky <gie1994@gmail.com>
2014-09-27 22:25:54 +0400

    Cosmetics

--- homework1/src/main.rs
+++ homework1/src/main.rs
@@ -2,7 +2,7 @@ use std::io::BufferedReader;
 use std::io::File;
 
 fn main() {
-   let path = Path::new("data-set.txt");
+    let path = Path::new("data-set.txt");
     let mut file = BufferedReader::new(File::open(&path));
     let lines: Vec<String> = file.lines().map(|x| x.unwrap()).collect();
     println!("{}", lines);
