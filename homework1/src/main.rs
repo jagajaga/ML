e7c813dcb940c97895da3ed00e6dacee23150c68
Arseniy Seroka <ars.seroka@gmail.com>
2014-09-27 22:20:49 +0400

    reorganize structure

--- homework1/src/main.rs
+++ homework1/src/main.rs
@@ -1,6 +1,9 @@
+use std::io::BufferedReader;
+use std::io::File;
+
 fn main() {
-    let x: int = 5;
-
-    println!("The value of x is: {}", x);
+   let path = Path::new("data-set.txt");
+    let mut file = BufferedReader::new(File::open(&path));
+    let lines: Vec<String> = file.lines().map(|x| x.unwrap()).collect();
+    println!("{}", lines);
 }
-
