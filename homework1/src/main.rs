7387d1023496ca0f4ff3431ca7df8db358d3287e
Ivan Gromakovsky <gie1994@gmail.com>
2014-09-28 17:25:28 +0400

    WIP

--- homework1/src/main.rs
+++ homework1/src/main.rs
@@ -2,46 +2,44 @@ use std::io::BufferedReader;
 use std::io::File;
 use std::rand::{task_rng, Rng};
 
-fn norm(numbers : Vec<f32>) -> f32 {
-    let mut result = 0f32;
+type Scalar = f64;
+type DataVec = Vec<(Point, bool)>;
+
+struct Point {
+    x : Scalar,
+    y : Scalar,
+}
+
+fn distance(a : Point, b : Point) -> Scalar {
+    let dx = a.x - b.x;
+    let dy = a.y - b.y;
+    (dx * dx + dy * dy).sqrt()
+}
+
+fn norm(numbers : Vec<Scalar>) -> Scalar {
+    let mut result : Scalar = 0.0;
     for &i in numbers.iter() {
         result += i;
     }
-    result / numbers.len() as f32
+    result / numbers.len() as Scalar
 }
 
-fn distance(a : (f32, f32), b : (f32, f32)) -> f32 {
-    let (x1, y1) = a;
-    let (x2, y2) = b;
-    ((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)).sqrt()
-}
-
-// 1nn
-fn simple(data : &Vec<[f32, .. 3]>, point : (f32, f32)) -> bool {
-    let mut min = std::f32::MAX_VALUE;
-    let mut value = false;
-    for v in data.iter() {
-        let d = distance(point, (v[0], v[1]));
-        if d < min {
-            min = d;
-            value = v[2] != 0f32;
-        }
-    }
-    value
+fn knn(data : &mut DataVec, k : uint, point : Point) -> bool {
+//    data.sort_by(|a, b| a.cmp(b, point));
+    false
 }
 
 fn main() {
     let path = Path::new("data/data-set.txt");
     let mut file = BufferedReader::new(File::open(&path));
-    let data : Vec<Vec<f32>> = file.lines().map(
+    let data : Vec<Vec<Scalar>> = file.lines().map(
         |x| x.unwrap().as_slice().trim_right_chars('\n').split(',').collect::<Vec<&str>>().iter().map(
-            |&x| from_str(x).expect("lolwhat")).collect::<Vec<f32>>()).collect();
-    let fst_norm = norm(data.iter().map(|x| x[0]).collect());
-    let snd_norm = norm(data.iter().map(|x| x[1]).collect());
-    let mut normalized_data : Vec<[f32, .. 3]> = data.iter().map(|x| [x[0] / fst_norm, x[1] / snd_norm, x[2]]).collect();
+            |&x| from_str(x).expect("lolwhat")).collect::<Vec<Scalar>>()).collect();
+    let norm0 = norm(data.iter().map(|x| x[0]).collect());
+    let norm1 = norm(data.iter().map(|x| x[1]).collect());
+    let mut normalized_data : DataVec = data.iter().map(|v| (Point { x: v[0] / norm0, y: v[1] / norm1 }, v[2] != 0.0) ).collect();
     let mut rng = task_rng();
     rng.shuffle(normalized_data.as_mut_slice());
-    println!("{}", simple(&normalized_data, (0.5, 0.4)));
-    println!("{}", normalized_data.iter().map(|x| x.as_slice()).collect::<Vec<&[f32]>>());
+    println!("{}", knn(&mut normalized_data, 2, Point {x: 0.5, y: 0.25} ));
 }
 
