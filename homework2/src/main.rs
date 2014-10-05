61916ca3c425627516c0793dbc49516060601733
Arseniy Seroka <ars.seroka@gmail.com>
2014-10-06 00:07:23 +0400

    add read

--- homework2/src/main.rs
+++ homework2/src/main.rs
@@ -4,32 +4,40 @@ use std::io::File;
 use std::io;
 
 #[deriving(PartialEq,Show,Clone)]
-struct Message<'a> {
-    subject : &'a[uint],
-    text : &'a[uint],
+struct Message {
+    subject : Vec<uint>,
+    text : Vec<uint>,
     is_spam : bool,
 }
 
-
 fn main() {
-    //let mut msgs_array = Vec::new();
+    let mut msgs_array = Vec::new();
     let folder = Path::new("data");
-    println!("{}", folder.as_str());
     for value in fs::walk_dir(&folder).unwrap() {
-        if value.is_file() {
-            let mut buf = BufferedReader::new(File::open(&value));
-            let mut file = buf.lines();
-            let is_spam = value.as_str().expect("No a file?").contains("spmsg");
-            let subject = file.next().map(|x| x.ok().expect("No values in subject").as_slice().trim_right_chars('\n').slice_from("Subject: ".len()).split(' ').filter(|x| !x.is_empty()).map(|x| from_str(x).expect("No numbers in subject")).collect::<Vec<uint>>()).collect::<Vec<Vec<uint>>>().as_slice().concat_vec(); 
-            let text = file.last().unwrap();
-            println!("{} {}", subject, text);
+        if value.is_dir() {
+            let mut temp = Vec::new();
+            for value in fs::walk_dir(&value).unwrap() {
+                if value.is_file() {
+                    let mut buf = BufferedReader::new(File::open(&value));
+                    let mut file = buf.lines();
+                    let is_spam = value.as_str().expect("No a file?").contains("spmsg");
+                    let subject = 
+                    file.next().map(
+                            |x| x.ok().unwrap().as_slice().trim_right_chars('\n').slice_from("Subject: ".len()).split(' ').filter(|x| !x.is_empty()).map(
+                                |x| from_str(x).expect("NaN in subject")).collect::<Vec<uint>>()
+                    ).unwrap();
+                    let text = 
+                    file.last().map(
+                            |x| x.ok().unwrap().as_slice().trim_right_chars('\n').split(' ').filter(|x| !x.is_empty()).map(
+                                |x| from_str(x).expect("NaN in text")).collect::<Vec<uint>>()
+                    ).unwrap();
+                    let message = Message {subject : subject, 
+                                        text : text, 
+                                        is_spam : is_spam};
+                    temp.push(message);
+                }
+            }
+            msgs_array.push(temp);
         }
     }
-
-    //let mut file = BufferedReader::new(File::open(&path));
-    //let data : Vec<Vec<Scalar>> = file.lines().map(
-        //|x| x.unwrap().as_slice().trim_right_chars('\n').split(',').collect::<Vec<&str>>().iter().map(
-            //|&x| from_str(x).expect("lolwhat")).collect::<Vec<Scalar>>()).collect();
-    //msgs_array.push(vec![Message{subject : vec![1], text : vec![2], is_spam : true}]);
-    //println!("{}", msgs_array);
 }
